237b53188d7e2ccb99c0a94dcb525a50fbabdb0a
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-08-26 10:58:29 +0900

    Title: 가장 작은 x 찾기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250826/가장 작은 x 찾기/find-smallest-x.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[10], b[10];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    for(int start=a[0];start<=b[0];start++){
+        int cur = 2 * start;
+        bool isValid = true;
+        for(int i=0;i<n;i++){
+            if(a[i] > cur || b[i] < cur) {
+                isValid = false;
+                break;
+            }
+            cur *= 2;
+        }
+        if(isValid){
+            cout<<start;
+            break;
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
