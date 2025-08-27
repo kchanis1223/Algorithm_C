ebcefd5c66c45d4bff16f696fb0f764c8a9bd63b
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-08-27 16:06:23 +0900

    Title: 훌륭한 점프, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250827/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, k;
+int arr[100];
+
+int main() {
+    cin >> n >> k;
+    vector<int> v;
+    v.push_back(0);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        v.push_back(arr[i]);
+    }
+
+    // Please write your code here.
+    for(int max = 1; max<=100; max++){
+        int cur = 1;
+        bool isValid = false;
+        while(cur<=n){
+            bool flag = false;
+            if(cur + k >= n) 
+            {
+                isValid = true;
+                break;
+            }
+            for(int i=1;i<=k;i++){
+                if(v[cur+i] <= max) {
+                    cur += i;
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) break;
+        }
+        if(isValid){
+            cout<<max;
+            break;
+        }
+    }
+    return 0;
+}
