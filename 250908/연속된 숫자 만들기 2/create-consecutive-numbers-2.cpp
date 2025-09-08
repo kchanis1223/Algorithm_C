f185050daa294f37d1d8c60d47774330d40411a3
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-08 17:33:14 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250908/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 250908/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -12,8 +12,8 @@ int main() {
     // Please write your code here.
     vector<int> v = {pos[0],pos[1],pos[2]};
     sort(v.begin(),v.end());
-    if(abs(pos[0] - pos[1]) == 2 || abs(pos[1] - pos[2]) == 2) cout<<1;
-    else if ( (pos[0]+ 1 == pos[1] && pos[1] + 1 == pos[2]) || (pos[0] - 1 == pos[1] && pos[1] - 1 == pos[2]) ) cout<<0;
+    if(abs(v[0] - v[1]) == 2 || abs(v[1] - v[2]) == 2) cout<<1;
+    else if ( (v[0]+ 1 == v[1] && v[1] + 1 == v[2]) || (v[0] - 1 == v[1] && v[1] - 1 == v[2]) ) cout<<0;
     else cout<<2;
     return 0;
 }
