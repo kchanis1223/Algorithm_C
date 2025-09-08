b119b73709d79d958e28a18b0087341e84a8413d
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-08 17:31:54 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250908/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 250908/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,6 +10,8 @@ int main() {
     for (int i = 0; i < 3; i++) cin >> pos[i];
 
     // Please write your code here.
+    vector<int> v = {pos[0],pos[1],pos[2]};
+    sort(v.begin(),v.end());
     if(abs(pos[0] - pos[1]) == 2 || abs(pos[1] - pos[2]) == 2) cout<<1;
     else if ( (pos[0]+ 1 == pos[1] && pos[1] + 1 == pos[2]) || (pos[0] - 1 == pos[1] && pos[1] - 1 == pos[2]) ) cout<<0;
     else cout<<2;
