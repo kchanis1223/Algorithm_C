d7d511802c9872ce6c4c7ddab7dfc9dbf4c5518c
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-08 11:16:12 +0900

    Title: 비둘기와 전기줄, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250908/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int N;
+int pigeon[100];
+int position[100];
+
+int main() {
+    cin >> N;
+    map<int,int> m;
+    for (int i = 0; i < N; i++) {
+        cin >> pigeon[i] >> position[i];
+    }
+
+    // Please write your code here.
+    int count = 0;
+    for(int i=0;i<N;i++){
+        if(m.find(pigeon[i]) != m.end()){
+            m[pigeon[i]] = position[i];
+        }
+        else if(position[i] != m[pigeon[i]]){
+            m[pigeon[i]] = position[i];
+            count +=1;
+        }
+    }
+    cout<<count;
+    return 0;
+}
