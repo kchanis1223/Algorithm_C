0d1eaad20f1855f3204a0f46700e9b7465eab32d
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-08 23:02:39 +0900

    Title: 독서실의 거리두기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250908/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+string seats;
+
+int main() {
+    cin >> N;
+    cin >> seats;
+
+    // Please write your code here.
+    int maxCount =0;
+    for(int i=0; i<N;i++){
+        
+        if(seats[i] == '1') continue;
+        string tmp = seats;
+        vector<int> v;
+        tmp[i] = '1';
+
+        int count =0;
+        for(int j=0;j<N;j++){
+            if(tmp[j] == '1' && j != 0){
+                v.push_back(count);
+                count = 0;      
+            }
+            else if(tmp[j] == '0'){
+                count +=1;
+            }
+        }
+        maxCount = max(maxCount , *min_element(v.begin(),v.end()) +1 );
+    }
+    cout<<maxCount;
+    return 0;
+}
