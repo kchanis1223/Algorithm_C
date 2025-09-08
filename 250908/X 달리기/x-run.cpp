00bbb6865823191ebb60a1f45765d9c8e93c1983
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-08 17:07:00 +0900

    Title: X 달리기, Time: 33ms, Memory: 39MB, Status: Passed - Codetree

--- 250908/X 달리기/x-run.cpp
+++ 250908/X 달리기/x-run.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <climits>
+#include<algorithm>
+#include <queue>
 using namespace std;
 
 int X;
@@ -8,57 +10,37 @@ int main() {
     cin >> X;
 
     // Please write your code here.
+    int minTime;
+    vector<int> dv = {-1,0,1};
+    vector<vector<int>> dp(10001,vector<int>(1000,INT_MAX)); // dp[위치][속도] = 시간
+    dp[1][2] = 1;
+    dp[1][1] = 1;
+    queue<pair<int,int>> q;
+    q.push({1,2});
+    q.push({1,1});
 
-    int minTime = INT_MAX;
-    for(int up = 1; up<300;up++){
-        for(int down = up; down <300;down++){
-            for(int maintain = down; maintain <300; maintain++){
-                bool isValid = true;
-                int speed = 1;
-                int cur = 0;
-                int time = 0;
+    while(!q.empty()){
+        auto [p,v] = q.front(); q.pop();
+        
+        for(int i=0;i<3;i++){
+            int new_v = v + dv[i];
+            if(new_v < 1) continue;
+            int new_p = p + v;
+            if(new_p > X) continue;
 
-                while(time < up){
-                    cur += speed;
-                    speed ++;
-                    time ++;
-                    if(cur > X){
-                        isValid = false;
-                        break;
-                    }
-                }
-                while(time < down){
-                    cur += speed;
-                    speed--;
-                    time ++;
-                    if(cur > X){
-                        isValid = false;
-                        break;
-                    }
-                }
-                while(time < maintain){
-                    cur += speed;
-                    time ++;
-                    if(cur > X){
-                        isValid = false;
-                        break;
-                    }
-                }
-                while(speed > 1){
-                    cur += speed;
-                    speed --;
-                    time ++;
-                    if(cur > X){
-                        isValid = false;
-                        break;
-                    }
-                }
-                if(!isValid) continue;
-                time += X - cur + 1;
-                if(minTime >time) minTime = time;
+            if(dp[new_p][new_v] > dp[p][v] + 1){
+                dp[new_p][new_v] = dp[p][v]+1;
+                q.push({new_p,new_v});
             }
+
         }
     }
-    cout << minTime << '\n';
+    // for(auto i : dp){
+    //     for(auto j:i){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    cout << dp[X][1]+1;
     return 0;
 }
