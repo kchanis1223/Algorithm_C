b2070fdcf0e226215cdecf7115421dadfeab9879
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-10 13:56:12 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250910/L, R 그리고 B/l-r-and-b.cpp
+++ 250910/L, R 그리고 B/l-r-and-b.cpp
@@ -7,6 +7,7 @@ char board[10][10];
 int main() {
     int Lx,Ly;
     int Bx,By;
+    int Rx,Ry;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             cin >> board[i][j];
@@ -16,10 +17,25 @@ int main() {
             if(board[i][j] == 'B'){
                 Bx = i ; By =j;
             }
+            if(board[i][j] == 'R'){
+                Rx = i ; Ry =j;
+            }
         }
     }
-
-    // Please write your code here.
-    cout<< abs(Bx-Lx) + abs(By - Ly) -1;
+    if(Lx == Bx && Lx == Rx){
+        if( (Ly < Ry && Ry < By) || (By < Ry && Ry < Ly)  ){
+            cout<< abs(Ly-By) + 1;
+        }
+        else cout<< abs(Ly-By) -1;
+    }
+    else if(Ly == By && Ly == Ry){
+        if( (Lx < Rx && Rx < Bx) || (Bx < Rx && Rx < Lx)  ){
+            cout<< abs(Lx-Bx) + 1;
+        }
+        else cout<< abs(Lx-Bx) -1;
+    }
+    else{
+        cout<< abs(Bx-Lx) + abs(By - Ly) -1;
+    }
     return 0;
 }
