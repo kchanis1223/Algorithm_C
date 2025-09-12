e913a1d1f1376f41e79705e04c9568457d6eb543
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-12 11:53:29 +0900

    Title: 수열의 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250912/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int sequence[100];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> sequence[i];
+    }
+    int idx =0;
+    for(int i=N-1;i>=0;i++){
+        if(sequence[i] < sequence[i-1]){
+            idx = i;
+            break;
+        }
+    }
+    cout<<idx;
+    // Please write your code here.
+
+    return 0;
+}
