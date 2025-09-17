88629cc00c85ae9b4469c5ca678393319e31f0c3
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-17 22:44:46 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250917/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
+++ 250917/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
-
+#include <vector>
+#include <climits>
 using namespace std;
 
 int n, m;
 int grid[5][5];
-vector<vector<bool>> visited(n,vector<bool>(m,false));
 
-
-
+bool checkRange(int x, int y, int r, int c){
+    if( x + r >= n || y + c >= m) return false;
+    return true;
+}
+int calSquare(int x, int y, int r, int c){
+    int sum =0;
+    for(int i = x;i<=x+r;i++){
+        for(int j =y;j<y+c;j++){
+            sum += grid[i][j];
+        }
+    }
+    return sum;
+}
+bool checkValid(int x , int y , int r, int c, vector<vector<bool>> &visited){
+    for(int i = x; i <=x+r;i++){
+        for(int j=y; j<=y+c; j++){
+            if ( visited[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 int main() {
     cin >> n >> m;
 
@@ -18,54 +39,49 @@ int main() {
     }
 
     vector<int> v;
-    int maxSum = 0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            int sum =0;
-            vector<int> tmp;
-            for(int r =1;r<n;r++){
-                for(int c =1;c<m;c++){
-                    if(checkRange(i,j,r,c) ) {
-                        int squareSum = calSquare(i,j,r,c);
-                        if(squareSum > sum) {
-                            sum = squareSum;
-                            tmp = {i,j,r,c};
+    int maxSum = INT_MIN;
+    for(int i1=0;i1<n;i1++){
+        for(int j1=0;j1<m;j1++){
+            for(int r1 = 0; r1<n; r1++){
+                for(int c1 =0; c1<m; c1++){
+                    if(checkRange(i1,j1,r1,c1)) {
+                        vector<vector<bool>> visited = vector<vector<bool>>(n,vector<bool>(m,false));
+                        int sum1 = 0;
+                        for(int i=i1; i<=i1+r1; i++){
+                            for(int j=j1; j<=j1+c1; j++){
+                                visited[i][j] = true;
+                                sum1 += grid[i][j];
+                            }
                         }
+                        for(int i2=0;i2<n;i2++){
+                            for(int j2=0;j2<m;j2++){
+                                for(int r2 = 0; r2<n; r2++){
+                                     for(int c2 =0; c2<m; c2++){
+                                        if(checkRange(i2,j2,r2,c2) && checkValid(i2,j2,r2,c2,visited)) {
+                                            int sum2 =0;
+                                            for(int i=i2;i<=i2+r2;i++){
+                                                for(int j=j2;j<=j2+c2;j++){
+                                                    sum2 += grid[i][j];
+                                                }
+                                            }
+                                            if(maxSum < sum1 + sum2) {
+                                                maxSum = sum1 + sum2;
+                                                // cout<<i1<<","<<j1<<","<<r1<<","<<c1<<endl;
+                                                // cout<<i2<<","<<j2<<","<<r2<<","<<c2<<endl;
+                                                // cout<<"sum1 :"<<sum1<< " sum2 : "<<sum2<<endl;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        
                     }
                 }
             }
-            if(maxSum < sum){
-                v = tmp;
-                maxSum = sum;
-            }
         }
     }
-    for(int i=v[0];i<v[0]+v[2];i++){
-        for(int j=v[1];j<v[1]+v[3];j++){
-            visited[i][j] = true;
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            int sum =0;
-            vector<int> tmp;
-            for(int r =1;r<n;r++){
-                for(int c =1;c<m;c++){
-                    if(checkRange(i,j,r,c) ) {
-                        int squareSum = calSquare(i,j,r,c);
-                        if(squareSum > sum) {
-                            sum = squareSum;
-                            tmp = {i,j,r,c};
-                        }
-                    }
-                }
-            }
-            if(maxSum < sum){
-                v = tmp;
-                maxSum = sum;
-            }
-        }
-    }
+    cout<<maxSum;
 
     // Please write your code here.
 
