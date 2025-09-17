96dd1743f516602d8827039222c5d5805d5ae3e2
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-17 17:06:27 +0900

    Title: 기울어진 직사각형, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250917/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int grid[20][20];
+
+bool checkValid(int x, int y, int i, int j){
+    if(x - i >= n) return false;
+    if( y + i >= n) return false;
+
+    if(x - i - j < 0) return false;
+    if( y + i - j < 0) return false;
+
+    if( y - j < 0) return false;
+
+    return true;
+}
+
+int calSum(int x , int y , int i, int j){
+    int sum = 0;
+    int curX = x;
+    int curY = y;
+    for(int k=1;k<=i;k++){
+        curX -=1;
+        curY +=1;
+        sum +=grid[curX][curY];
+    }
+    for(int k=1;k<=j;k++){
+        curX -=1;
+        curY -=1;
+        sum +=grid[curX][curY];
+    }
+    for(int k=1;k<=i;k++){
+        curX +=1;
+        curY -=1;
+        sum +=grid[curX][curY];
+    }
+    for(int k=1;k<=i;k++){
+        curX +=1;
+        curY +=1;
+        sum +=grid[curX][curY];
+    }
+    return sum;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    int result = 0;
+    for(int x =0;x<n;x++){
+        for(int y =0;y<n;y++){
+            int maxSum = grid[x][y];
+            for(int i=1;i<n;i++){
+                for(int j=1;j<n;j++){
+                    if(checkValid(x,y,i,j)){
+                        int sum = calSum(x,y,i,j);
+                        if(maxSum < sum) maxSum = sum;
+                    }
+                }
+            }
+            if(result < maxSum) result = maxSum;
+        }
+    }
+    cout<<result;
+    // Please write your code here.
+
+    return 0;
+}
