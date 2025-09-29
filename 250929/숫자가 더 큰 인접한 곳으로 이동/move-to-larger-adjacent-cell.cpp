7dfec29f7561bb141e40d249c3208c17c742858c
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-09-29 17:49:34 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250929/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
+++ 250929/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -7,6 +7,10 @@ int n;
 int r, c;
 int a[100][100];
 
+bool checkRange(int x, int y){
+    if(x >= 0 && x < n && y >=0 && y <n) return true;
+    return false;
+}
 int main() {
     cin >> n >> r >> c;
 
@@ -21,13 +25,14 @@ int main() {
     int curX = r;
     int curY = c;
     bool isMoved  = true;
-    vector<int> result= {a[r][c]};
+    
+    vector<int> result= {a[curX][curY]};
     while(isMoved){
         isMoved = false;
         for(int k = 0 ; k<4; k++){
             int nxtX = curX + dx[k];
             int nxtY = curY + dy[k];
-            if(a[curX][curY] < a[nxtX][nxtY]){
+            if( checkRange(nxtX,nxtY) && a[curX][curY] < a[nxtX][nxtY]){
                 isMoved = true;
                 curX = nxtX;
                 curY = nxtY;
