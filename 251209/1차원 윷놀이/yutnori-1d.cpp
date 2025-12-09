f4862bf8c4f6073aecab21dfb9540f7689ea877f
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-12-09 17:28:17 +0900

    Title: 1차원 윷놀이, Time: 1093ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 251209/1차원 윷놀이/yutnori-1d.cpp
+++ 251209/1차원 윷놀이/yutnori-1d.cpp
@@ -5,14 +5,25 @@ using namespace std;
 int n, m, k;
 int nums[12];
 
-vector<vector<int>> all_moves;
+int maxCount = 0;
 
+int calCount(vector<int> v){
+    int count = 0;
+    vector<int> k_positions(k+1,1);
+    for(int i=0;i<v.size();i++){
+        k_positions[v[i]] += nums[i];
+    }
+    for(auto kp : k_positions){
+        if(kp >= m) count ++;
+    }
+    return count;
+}
 void backtracking(vector<int> &v){
-    if(v.size()== n){
-        all_moves.push_back(v);
+    if(calCount(v) > maxCount) maxCount = calCount(v);
+    if(v.size()==n){
         return;
     }
-    for(int select = 1;select<=k;select++){
+    for(int select = 1;select<=4;select++){
         v.push_back(select);
         backtracking(v);
         v.pop_back();
@@ -22,33 +33,11 @@ void backtracking(vector<int> &v){
 int main() {
     cin >> n >> m >> k;
 
-    int maxCount = 0;
-
     for (int i = 0; i < n; i++) {
         cin >> nums[i];
     }
-    vector<int> v= {};
+    vector<int> v = {};
     backtracking(v);
-
-    // for(auto i : all_moves){
-    //     for(auto j : i){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    // cout<<endl;
-
-    for(auto moves : all_moves){
-        int count =0;
-        vector<int> k_positions(k+1,1);
-        for(int i=0; i<moves.size();i++){
-            k_positions[moves[i]] += nums[i];
-        }
-        for(auto kp : k_positions){
-            if (kp >= m) count ++;
-        }
-        if(maxCount < count) maxCount = count;
-    }
     cout<<maxCount;
     // Please write your code here.
 
