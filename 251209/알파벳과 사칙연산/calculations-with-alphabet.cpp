60d7b221cb4b00604794c2e04bf1054f725d7971
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-12-09 15:51:27 +0900

    Title: 알파벳과 사칙연산, Time: 11ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251209/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int N;
+// void backtracking(int idx, vector<int> &v){
+//     if(idx == v.size()){
+//         total_abcdef.push_back(v);
+//         return;
+//     }
+//     for(int i=idx;i<v.size();i++){
+//         v[i]
+//     }
+// }
+int cal(vector<char>alphabets, vector<char> operators, vector<int> abcdef){
+
+    map<char,int> m;
+    m['a'] = abcdef[0]; m['b'] = abcdef[1]; m['c'] = abcdef[2];
+    m['d'] = abcdef[3]; m['e'] = abcdef[4]; m['f'] = abcdef[5];
+
+    // for(auto i : m) cout<<i.first<<"="<<i.second<<" ";
+    // cout<<endl;
+    
+    int cur = m[alphabets[0]];
+    for(int i=1;i<=N/2;i++){
+        if(operators[i-1] == '+'){
+            cur += m[alphabets[i]];
+        }
+        else if(operators[i-1] == '-'){
+            cur -= m[alphabets[i]];
+        }
+        else{
+            cur *= m[alphabets[i]];
+        }
+        //cout<<"cur :"<<cur<<" ";
+    }
+    //cout<<endl;
+    //cout<<"cal :"<<cur<<endl;
+    return cur;
+}
+
+int main() {
+    // Please write your code here.
+    string input;
+    cin>>input;
+    N = input.size();
+    vector<char> alphabets;
+    vector<char> operators;
+    for(auto i : input){
+        if(i == '+' || i =='-' || i =='*' ) operators.push_back(i);
+        else {
+            alphabets.push_back(i);
+        }
+    }
+    vector<vector<int>> total_abcdef;
+    for(int a=1;a<=4;a++){
+        for(int b=1;b<=4;b++){
+            for(int c=1;c<=4;c++){
+                for(int d=1;d<=4;d++){
+                    for(int e=1;e<=4;e++){
+                        for(int f=1;f<=4;f++){
+                            total_abcdef.push_back({a,b,c,d,e,f});
+                        }
+                    }
+                }
+            }
+        }
+    }
+    int maxResult = 0;
+
+    for(auto abcdef : total_abcdef){
+        int result = cal(alphabets,operators,abcdef);
+        if(maxResult < result) maxResult = result;
+    }
+    cout<< maxResult;
+
+    return 0;
+}
