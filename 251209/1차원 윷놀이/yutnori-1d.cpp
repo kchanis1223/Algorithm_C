ffdb93aeb02b0c7aae8c3621063943b2228e8afb
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2025-12-09 17:49:01 +0900

    Title: 1차원 윷놀이, Time: 286ms, Memory: 0MB, Status: Passed - Codetree

--- 251209/1차원 윷놀이/yutnori-1d.cpp
+++ 251209/1차원 윷놀이/yutnori-1d.cpp
@@ -6,27 +6,30 @@ int n, m, k;
 int nums[12];
 
 int maxCount = 0;
+vector<int> kp;
 
-int calCount(vector<int> v){
+int calCount(vector<int> &kp){
     int count = 0;
-    vector<int> k_positions(k+1,1);
-    for(int i=0;i<v.size();i++){
-        k_positions[v[i]] += nums[i];
-    }
-    for(auto kp : k_positions){
-        if(kp >= m) count ++;
+    for(auto p : kp){
+        if(p >= m) count ++;
     }
     return count;
 }
 void backtracking(vector<int> &v){
-    if(calCount(v) > maxCount) maxCount = calCount(v);
-    if(v.size()==n){
+    
+    int count = calCount(kp);
+    if(count > maxCount) maxCount = count;
+    if(v.size() == n){
         return;
     }
-    for(int select = 1;select<=4;select++){
+    for(int select = 1;select<=k;select++){
+        if(kp[select] >= m) continue;
         v.push_back(select);
+        kp[select] += nums[v.size()-1];
         backtracking(v);
+        kp[select] -= nums[v.size()-1];
         v.pop_back();
+        
     }
 }
 
@@ -37,6 +40,7 @@ int main() {
         cin >> nums[i];
     }
     vector<int> v = {};
+    kp = vector<int>(k+1,1);
     backtracking(v);
     cout<<maxCount;
     // Please write your code here.
