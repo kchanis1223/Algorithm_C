1229d81e22eb7c667c293c245320f90aec2d873f
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-07 11:34:56 +0900

    Title: 빙하, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260107/빙하/glacier.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+int a[200][200];
+
+vector<int> dx = {0,0,-1,1};
+vector<int> dy = {-1,1,0,0};
+
+bool checkRange(int x , int y){
+    return x>=0 && x <N && y >=0 && y < M ? true: false;
+}
+bool checkMelting(int x, int y){
+    bool isMelt = false;
+    for(int i=0;i<4;i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(checkRange(nx,ny) && a[nx][ny] == 1) continue;
+
+        bool isAlone = true;
+        for(int j=0;j<4;j++){
+            int nnx = nx + dx[j];
+            int nny = ny + dy[j];
+            if(checkRange(nnx,nny) && a[nnx][nny] == 0) isAlone = false;
+        }
+
+        if(isAlone) continue;
+
+        isMelt = true;
+    }
+    return isMelt;
+}
+
+int main() {
+    cin >> N >> M;
+
+    int init =0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> a[i][j];
+            if(a[i][j] == 1) init += 1;
+        }
+    }
+
+    vector<int> remainings = {init};
+    for(int t = 0; t<=100; t++){
+        vector<vector<bool>> grid = vector<vector<bool>>(N,vector<bool>(M,false));
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(a[i][j] == 1) grid[i][j] = true;
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(a[i][j] == 1 && checkMelting(i,j)){
+                    grid[i][j] = false;
+                }
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(!grid[i][j]){
+                    a[i][j] = 0;
+                }
+            }
+        }
+        int remain = 0;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                //cout<<a[i][j]<<" ";
+                if(a[i][j] == 1) remain += 1;
+            }
+            //cout<<endl;
+        }
+        //cout<<endl;
+
+        //cout<<"t : "<<t<<" / remain :"<<remain<<endl;
+        if(remain == 0) break;
+        else remainings.push_back(remain);
+    }
+    //for(auto i : remainings) cout<<i<<" ";
+    cout<<remainings.size()<<" "<<remainings.back();
+    // Please write your code here.
+
+    return 0;
+}
