e66c9778fea517db354b76e3c4536e71b670e0cd
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-08 14:11:02 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 35ms, Memory: 15MB, Status: Passed - Codetree

--- 260108/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 260108/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
 int N;
 bool checkRange(int x){
-    return x >= 0 && x <= 2*N ? true: false;
+    return x >= 0 && x <= 1000100 ? true: false;
 }
 int main() {
     cin >> N;
 
     // Please write your code here.
     queue<pair<int,int>> q; // value/d
-    vector<bool> visited = vector<bool>(2*N+1,false);
+    vector<bool> visited = vector<bool>(100000000,false);
     q.push({1,0});
     visited[1] = true;
 
     while(!q.empty()){
+        
         int value = q.front().first;
         int d = q.front().second;
         q.pop();
+
+        //cout<<"value : "<< value<<" / d : "<<d<<endl;
         if(value == N){
             cout<<d;
             break;
         }
-        if(checkRange(value+1))  {
+        if(checkRange(value+1) && !visited[value+1])  {
             q.push({value+1,d+1});
             visited[value+1] = true;
         }
-        if(checkRange(value-1))  {
+        if(checkRange(value-1) && !visited[value-1])  {
             q.push({value-1,d+1});
             visited[value-1] = true;
         }
-        if(checkRange(2*value))  {
+        if(checkRange(2*value) && !visited[value*2])  {
             q.push({value*2,d+1});
             visited[value*2] = true;
         }
-        if(checkRange(3*value))  {
+        if(checkRange(3*value) && !visited[value*3])  {
             q.push({value*3,d+1});
             visited[value*3] = true;
         }
