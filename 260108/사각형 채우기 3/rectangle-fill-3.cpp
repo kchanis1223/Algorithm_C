38b9c627755b7055929be0e2a7d1a7c35545596e
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-08 17:57:59 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260108/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    vector<int> dp = vector<int>(n+1,0);
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+    
+    for(int i=3;i<=n;i++){
+        int exception = 0;
+        for(int k=3; k<=i;k++){
+            exception += dp[i-k];
+        }
+        dp[i] = dp[i-1] * 2 + dp[i-2] * 3 + 2*exception ;
+        dp[i] %= 1000000007;
+    }
+
+    cout<< dp[n];
+    return 0;
+}
