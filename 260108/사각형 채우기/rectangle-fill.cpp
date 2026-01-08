fad8a80098e896064ce976aff785a68351733a0a
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-08 17:15:21 +0900

    Title: 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    vector<int> dp = vector<int>(n+1,0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i=3;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] %= 10007;
+    }
+    cout<<dp[n];
+    return 0;
+}
