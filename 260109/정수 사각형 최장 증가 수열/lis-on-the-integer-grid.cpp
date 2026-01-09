e795cdac3c2a47db8e587083e89c815b53c457ea
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-09 17:31:22 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 96ms, Memory: 2MB, Status: Passed - Codetree

--- 260109/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 260109/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -13,48 +13,25 @@ vector<int> dy = {-1,1,0,0};
 bool checkRange(int x, int y){
     return x >=0 && x < n && y >=0 && y < n ? true:false;
 }
-struct Node{
-    int x,y,d;
-    Node(int x, int y, int d) : x(x) , y(y), d(d) {}
-};
 
 vector<vector<int>> dp;
 
-int bfs(int x, int y){
-    vector<vector<bool>> visited = vector<vector<bool>>(n,vector<bool>(n,false));
-    queue<Node> q;
-    q.push(Node(x,y,1));
-    visited[x][y] = true;
+int dfs(int x, int y){
+    if(dp[x][y] != 0) return dp[x][y];
 
-    int maxCount = 0;
-    while(!q.empty()){
-        bool isLast = true;
-        int x = q.front().x;
-        int y = q.front().y;
-        int d = q.front().d;
-        q.pop();
-
-        if(dp[x][y] != 0){
-            if(maxCount < d + dp[x][y] - 1) maxCount = d + dp[x][y] - 1 ;
-            break;
+    dp[x][y] = 1;
+    for(int i=0;i<4;i++){
+        int nx = x +dx[i];
+        int ny = y +dy[i];
+        if(checkRange(nx,ny) && grid[nx][ny] < grid[x][y] ){
+            dp[x][y] = max(dp[x][y], 1 + dfs(nx,ny));
         }
-        for(int i=0;i<4;i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(checkRange(nx,ny) && !visited[nx][ny] && (grid[nx][ny] > grid[x][y] ) ){
-                isLast = false;
-                q.push(Node(nx,ny,d+1));
-                visited[nx][ny] = true;
-            }
-        }
-        if(isLast && maxCount < d) maxCount = d; 
     }
-    return maxCount;
+    return dp[x][y];
 }
 
 int main() {
     cin >> n;
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
@@ -67,19 +44,18 @@ int main() {
     int result = 0;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(dp[i][j] != 0 ) continue;
-            int cnt = bfs(i,j);
-            dp[i][j] = cnt;
-            if(result < cnt) result = cnt;
+            result = max(result, dfs(i,j));
         }
     }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(result < dp[i][j]) result = dp[i][j];
+            //cout<<dp[i][j]<<" ";
+        }
+        //cout<<endl;
+    }
     cout<<result;
-    // cout<<endl;
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<n; j++){
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+
     return 0;
 }
