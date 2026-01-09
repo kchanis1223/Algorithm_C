cc31246e3533067822b4a54f55ea3baeff2eff6a
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-09 16:40:48 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1094ms, Memory: 2MB, Status: Time Limit Exceed - Codetree

--- 260109/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 260109/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -11,12 +11,14 @@ vector<int> dx = {0,0,-1,1};
 vector<int> dy = {-1,1,0,0};
 
 bool checkRange(int x, int y){
-    return x >=0 && x <= n && y >=0 && y < n ? true:false;
+    return x >=0 && x < n && y >=0 && y < n ? true:false;
 }
 struct Node{
     int x,y,d;
     Node(int x, int y, int d) : x(x) , y(y), d(d) {}
 };
+
+vector<vector<int>> dp;
 
 int bfs(int x, int y){
     vector<vector<bool>> visited = vector<vector<bool>>(n,vector<bool>(n,false));
@@ -32,10 +34,14 @@ int bfs(int x, int y){
         int d = q.front().d;
         q.pop();
 
+        if(dp[x][y] != 0){
+            if(maxCount < d + dp[x][y] - 1) maxCount = d + dp[x][y] - 1 ;
+            break;
+        }
         for(int i=0;i<4;i++){
             int nx = x+dx[i];
             int ny = y+dy[i];
-            if(checkRange(nx,ny) && (grid[nx][nx] > grid[x][y] ) && !visited[nx][ny] ){
+            if(checkRange(nx,ny) && !visited[nx][ny] && (grid[nx][ny] > grid[x][y] ) ){
                 isLast = false;
                 q.push(Node(nx,ny,d+1));
                 visited[nx][ny] = true;
@@ -56,15 +62,24 @@ int main() {
     }
 
     // Please write your code here.
-    //vector<vector<int>> dp = vector<vector<int>>(n,vector<int>(n,0));
+    dp = vector<vector<int>>(n,vector<int>(n,0));
 
     int result = 0;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
+            if(dp[i][j] != 0 ) continue;
             int cnt = bfs(i,j);
+            dp[i][j] = cnt;
             if(result < cnt) result = cnt;
         }
     }
     cout<<result;
+    // cout<<endl;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     return 0;
 }
