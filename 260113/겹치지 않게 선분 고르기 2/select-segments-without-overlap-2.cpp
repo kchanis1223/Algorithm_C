0d258f27377ab22ad803f10042f46a72579d6b49
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-13 17:17:55 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260113/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int x1[1000];
+int x2[1000];
+
+int main() {
+    cin >> n;
+    vector<pair<int,int>> v;
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+        v.push_back({x1[i],x2[i]});
+    }
+
+    // Please write your code here.
+    sort(v.begin(),v.end());
+    vector<int> dp = vector<int>(n,0); // i번째 선분을 마지막으로 선택했을때의 최대 선택 가능 선분의 갯수
+    dp[0] = 1;
+    for(int i=1;i<n;i++){
+        for(int j=0; j<i;j++){
+            if(v[j].second < v[i].first) dp[i] = max(dp[i],dp[j] + 1);
+        }
+    }
+    cout<<*max_element(dp.begin(),dp.end());
+    return 0;
+}
