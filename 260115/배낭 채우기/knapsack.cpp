72a7a7ad9590defb25e9a27a2f6196cbc4df7076
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-15 15:16:10 +0900

    Title: 배낭 채우기, Time: 9ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260115/배낭 채우기/knapsack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int w[100], v[100];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> w[i] >> v[i];
+    }
+    vector<vector<int>> dp = vector<vector<int>>(N,vector<int>(M+1,0)); 
+    // dp[보석 종류i][무게w] = 최대 가치 , i 보석 종류까지 선택했을때 t 무게이상일때의 따른 최대 가치
+    dp[0][w[0]] = v[0];
+    
+    for(int i =1;i<N;i++){
+        int weight = w[i];
+        int value = v[i];
+        for(int k=0;k<=M;k++){
+            if(k - weight >= 0) dp[i][k] = max(dp[i-1][k], dp[i-1][k-weight]+value);
+        }
+    }
+
+    int result = 0;
+    for(auto i : dp){
+        for(auto j : i) result = max(result,j); //cout<<j<<" ";
+        //cout<<endl;
+    }
+    cout<<result;
+    // Please write your code here.
+
+    return 0;
+}
