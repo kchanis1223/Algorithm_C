005cbc52f1bfaba048a6d7884248cd07ec3af43d
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-01-15 17:52:45 +0900

    Title: 최소 차 분할, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260115/최소 차 분할/minimum-diff-partition.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        total += arr[i];
+    }
+
+    vector<bool> dp = vector<bool>(total+1,false);
+    dp[arr[0]] = true;
+    for(int s = arr[0]+1; s<=total; s++){
+        for(int i=0;i<n;i++){
+            if(!dp[s-arr[i]]) dp[s] = true;
+        }
+    }
+
+    //for(auto i :dp) cout<<i<<" ";
+    //cout<<endl;
+    int minValue = INT_MAX;
+    for(int i = 1 ; i<total;i++){
+        if(dp[i])minValue = min(minValue, abs(total - 2*i));
+    }
+    cout<<minValue;
+
+
+    // vector<vector<int>> dp = vector<vector<int>>(n+1,vector<int>(100000,INT_MAX)); 
+    // //dp[i][t] 는 i번째까지 그룹을 나눴을때 A그룹이 t일때 "A-나머지"가 최소인 값
+    // dp[0][v[0]] = abs(total - v[0] - v[0]);
+    // for(int i=1;i<=n;i++){
+    //     int value = v[i];
+    //     for(int t=0; t<=100000; t++){
+    //         if(t - value > 0 && dp[i][t-value] != INT_MAX) dp[i][t] = dp[t]
+    //     }
+    // }
+     return 0;
+}
