b04fd7009cc1a367de71c1937490220708443189
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-09 15:56:28 +0900

    Title: 올바른 등식 만들기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260209/올바른 등식 만들기/right-equality.cpp
+++ 260209/올바른 등식 만들기/right-equality.cpp
@@ -17,8 +17,8 @@ int main() {
     // dp[i][j] 는 i번째 까지 진행하였을때 현재 합이 j-20인 경우의 경우의 수
 
     dp[0][0] = 0;
-    dp[1][nums[0]+20] = 1;
-    dp[1][-nums[0]+20] = 1;
+    dp[1][nums[0]+20] += 1;
+    dp[1][-nums[0]+20] += 1;
     for(int i=1 ; i< N; i++){
         for(int j=0 ; j<=40; j++){
             if(j - nums[i]  < 0 ) dp[i+1][j] = dp[i][j+nums[i]];
@@ -27,13 +27,13 @@ int main() {
 
         }
     }
-    for(auto i : dp){
-        for(auto j : i){
-            //cout<<j<<" ";
-        }
-        //cout<<endl;
-    }
-    //cout<<endl;
+    // for(auto i : dp){
+    //     for(auto j : i){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<endl;
     cout<<dp[N][M+20];
     // Please write your code here.
 
