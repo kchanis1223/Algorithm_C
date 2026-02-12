c4c1edfb51bad0f3f5f54ef1a9aa8698d79c20ce
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-12 11:28:03 +0900

    Title: 두 수의 합, Time: 1096ms, Memory: 8MB, Status: Time Limit Exceed - Codetree

--- 260212/두 수의 합/sum-of-two-num.cpp
+++ 260212/두 수의 합/sum-of-two-num.cpp
@@ -6,19 +6,24 @@ using namespace std;
 int n, k;
 int arr[100000];
 
-map<int,int> m; // 값,인덱스
+map<int,vector<int>> m; // 값,인덱스
 int main() {
     cin >> n >> k;
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        m[arr[i]] = i+1;
+        m[arr[i]].push_back(i);
     }
     int result = 0;
     for(int i=0;i<n;i++){
-        if(m[k - arr[i]] != 0 && m[k - arr[i]] != i+1) {
-            //cout<<i<<endl;
-            result += 1;
+        for(auto [value, v] : m){
+            if(arr[i] + value != k) continue;
+            if(!v.empty()) {
+                for(auto idx : v){
+                    if(idx != i) result += 1;
+                    //cout<<i<<","<<idx<<endl;
+                }
+            }
         }
     }
     cout<<result/2;
