ff0d19b20706fdfe59ebbf5efc61b2d40ad675ff
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-19 13:54:48 +0900

    Title: 낮은 지점들, Time: 68ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 260219/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int n;
+int x[100000], y[100000];
+
+int main() {
+    cin >> n;
+    map<int,int> m;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+        if( m[x[i]] == 0 ) m[x[i]] = y[i];
+        else{
+            if(m[x[i]] > y[i]) m[x[i]] = y[i];
+        }
+    }
+    int sum =0;
+    for(auto i : m){
+        //cout<<i.first<<","<<i.second<<endl;
+        sum += i.second;
+    }
+    cout<<sum;
+    // Please write your code here.
+
+    return 0;
+}
