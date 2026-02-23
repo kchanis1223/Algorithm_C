d4e17c93f836739ede16480562eb36e935114ecd
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-23 16:39:17 +0900

    Title: C-TAG, Time: 1503ms, Memory: 0MB, Status: Passed - Codetree

--- 260223/C-TAG/c-tag.cpp
+++ 260223/C-TAG/c-tag.cpp
@@ -14,17 +14,18 @@ int main() {
 
     for (int i = 0; i < n; i++) cin >> B[i];
 
-    map<vector<int>,set<string>> ms;
+    //map<vector<int>,set<string>> ms;
     int result =0;
     for(int i=0;i<m-2;i++){
         for(int j=i+1;j<m-1;j++){
             for(int k=j+1;k<m;k++){
+                set<string> s;
                 for(int t=0;t<n;t++){
                     string tmp = "";
                     tmp += A[t][i];
                     tmp += A[t][j];
                     tmp += A[t][k];
-                    ms[{i,j,k}].insert(tmp);
+                    s.insert(tmp);
                 }
                 bool isValid = true;
                 for(int t=0; t<n; t++){
@@ -32,8 +33,8 @@ int main() {
                     tmp += B[t][i];
                     tmp += B[t][j];
                     tmp += B[t][k];
-                    auto it = ms[{i,j,k}].find(tmp);
-                    if(it != ms[{i,j,k}].end()){
+                    auto it = s.find(tmp);
+                    if(it != s.end()){
                         isValid = false;
                         break;
                     }
