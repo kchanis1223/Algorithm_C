60a4b28e37d059826330ced3f63bdae88125e80a
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-23 15:07:10 +0900

    Title: 단어장, Time: 9ms, Memory: 3MB, Status: Wrong Answer - Codetree

---
+++ 260223/단어장/word-list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int n;
+string words[100000];
+
+int main() {
+    cin >> n;
+    map<string,int> m;
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+        m[words[i]] ++;
+    }
+    for(auto i : m){
+        cout<<i.first<<" "<<i.second;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
