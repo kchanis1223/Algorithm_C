044f37401e201a3767354a7a9c75dab948d258b6
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-23 14:58:37 +0900

    Title: 처음 등장하는 위치, Time: 78ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 260223/처음 등장하는 위치/first-appearing-position.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int main() {
+    cin >> n;
+    map<int,int> m;
+    int order = 1;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(m.find(arr[i]) == m.end()) {
+            m[arr[i]] = order;
+            order ++;
+        }
+    }
+    for(auto i : m){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    // Please write your code here.
+
+    return 0;
+}
