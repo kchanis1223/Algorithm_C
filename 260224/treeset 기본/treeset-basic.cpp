377b90e58904a5087c4bf6e9d5cf1628d395a7fb
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-24 17:25:12 +0900

    Title: treeset 기본, Time: 13ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260224/treeset 기본/treeset-basic.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+// 변수 선언
+int n;
+set<int> s;
+
+int main() {
+    // 입력:
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+
+        int x;
+
+        if(command == "add") {
+            cin >> x;
+            s.insert(x);
+        }
+        else if(command == "remove") {
+            cin >> x;
+            s.erase(x);
+        }
+        else if(command == "find") {
+            cin >> x;
+            if(s.find(x) != s.end())
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        else if(command == "lower_bound") {
+            cin >> x;
+            if(s.lower_bound(x) != s.end())
+                cout << *s.lower_bound(x) << endl;
+            else
+                cout << "None" << endl;
+        }
+        else if(command == "upper_bound") {
+            cin >> x;
+            if(s.upper_bound(x) != s.end())
+                cout << *s.upper_bound(x) << endl;
+            else
+                cout << "None" << endl;
+        }
+        else if(command == "largest") {
+            if(s.rbegin() != s.rend())
+                cout << *s.rbegin() << endl;
+            else
+                cout << "None" << endl;
+        }
+        else {
+            if(s.begin() != s.end())
+                cout << *s.begin() << endl;
+            else
+                cout << "None" << endl;
+        }
+    }
+    return 0;
+}
