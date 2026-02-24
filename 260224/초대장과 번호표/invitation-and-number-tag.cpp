421455eb556c8b1657b43782cfcc0dcd8e64252f
kchanis1223 <48437147+kchanis1223@users.noreply.github.com>
2026-02-24 16:25:11 +0900

    Title: 초대장과 번호표, Time: 379ms, Memory: 24MB, Status: Passed - Codetree

--- 260224/초대장과 번호표/invitation-and-number-tag.cpp
+++ 260224/초대장과 번호표/invitation-and-number-tag.cpp
@@ -15,7 +15,7 @@ int main() {
 
     vector<set<int>> groups = vector<set<int>>(G,set<int>()); map<int,vector<int>> m; 
     // m[i] = i가 포함되어 있는 전체 그룹 
-    for (int i = 0; i < G; i++) { cin >> group_size[i]; for (int j = 0; j < group_size[i]; j++) { cin >> group[i][j]; groups[i].insert(group[i][j]); m[group[i][j]].push_back(i); } } queue<int> q; vector<int> v; q.push(1); while(!q.empty()){ int num = q.front(); v.push_back(num); q.pop(); for(auto g : m[num]){ groups[g].erase(num); if(groups[g].size() == 1) q.push(*groups[g].begin()); } } cout<<v.size();
+    for (int i = 0; i < G; i++) { cin >> group_size[i]; for (int j = 0; j < group_size[i]; j++) { cin >> group[i][j]; groups[i].insert(group[i][j]); m[group[i][j]].push_back(i); } } queue<int> q; set<int> v; q.push(1); while(!q.empty()){ int num = q.front(); v.insert(num); q.pop(); for(auto g : m[num]){ groups[g].erase(num); if(groups[g].size() == 1) q.push(*groups[g].begin()); } } cout<<v.size();
 
     return 0;
 }
